ee9435d8884d20db241fb32c8a5de0968d15758b
Meno <menolycasch2006@gmail.com>
2026-08-09 14:14:16 +0530

    Commit 3

--- app.js
+++ app.js
@@ -1,2 +1,38 @@
+const apiKey = "fa66d55bfb24497380e85722243011";
+const baseUrl = "http://api.weatherapi.com/v1";
+
+
+function displayWeather(data) { // Display weather info
+
+    document.getElementById("contentSection").innerHTML = `
+
+        <div class="weather-info">
+
+            <img 
+                class="weather-icon"
+                src="https:${data.current.condition.icon}" 
+                alt="${data.current.condition.text}"
+            >
+
+            <h2>${data.location.name}</h2>
+
+            <p class="country">
+                ${data.location.country}
+            </p>
+
+            <div class="temperature">
+                ${data.current.temp_c}°C
+            </div>
+
+            <p class="condition">
+                ${data.current.condition.text}
+            </p>
+
+        </div>
+
+    `;
+}
+
+
 
 
